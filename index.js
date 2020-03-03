bfb5eb11d234ceab0cc1e3757dfbd0b03c70459a
Dorian Andrés <dcortes@builderhomesite.com>
2020-03-03 16:57:08 -0600

    index included

---
+++ index.js
@@ -0,0 +1,4 @@
+import { ContextProvider } from "./modules/lit-context/context-provider";
+import { contextConsumer } from "./modules/lit-context/contextConsumer";
+
+export default { ContextProvider, contextConsumer };
