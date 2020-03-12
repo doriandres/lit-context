9b42af28559537efd7b5ec2ba452a03549d68648
Gary Valverde Hampton <garyhampton.12@gmail.com>
2020-03-11 18:58:05 -0600

    feat(build): tsdx

--- src/decorators/consume.ts
+++ src/decorators/consume.ts
@@ -1,5 +1,4 @@
 import { contextConsumer } from "../lib/context-consumer";
-import { LitElement } from "lit-element";
 
 /**
  * @param { Map } changed
@@ -7,7 +6,7 @@ import { LitElement } from "lit-element";
 export const consume = (providerTag: string, mapFn?: Function) => {
     const consumer = (consumer: Element, mapFn?: Function) => contextConsumer(providerTag, consumer, mapFn);
 
-    return function<T extends { new(...args: any[]): LitElement }> (constructor: T) {
+    return function<T extends { new(...args: any[]): HTMLElement }> (constructor: T) {
         return class extends constructor {
             connectedCallback() {
                 super.connectedCallback();
