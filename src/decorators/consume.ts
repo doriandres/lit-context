54c889b2ca471c5414430958b3c92ccc044a7e81
Gary Valverde Hampton <garyhampton.12@gmail.com>
2020-04-02 19:52:27 -0600

    fix(consume): suport legacy and standard decorators

--- src/decorators/consume.ts
+++ src/decorators/consume.ts
@@ -8,21 +8,64 @@
 
 import { contextConsumer } from "../lib/context-consumer";
 import { MapFn } from "../types/context";
-import { LitElement } from "lit-element";
 
 /**
  * @param providerTag Provider tag name
  * @param mapFn  Function to map provider value
  */
-export const consume = <T = object>(providerTag: string, mapFn?: MapFn<T>) => {
-    const consumer = (consumer: LitElement, mapFn?: MapFn<T>) => contextConsumer(providerTag, consumer, mapFn);
+export type Constructor<T> = {
+    // tslint:disable-next-line:no-any
+    new(...args: any[]): T
+};
 
-    return function <T extends { new(...args: any[]): LitElement }>(classConstructor: T) {
+// From the TC39 Decorators proposal
+interface ClassDescriptor {
+    kind: 'class';
+    elements: ClassElement[];
+    finisher?: <T>(clazz: Constructor<T>) => undefined | Constructor<T>;
+}
+
+// From the TC39 Decorators proposal
+interface ClassElement {
+    kind: 'field' | 'method';
+    key: PropertyKey;
+    placement: 'static' | 'prototype' | 'own';
+    initializer?: Function;
+    extras?: ClassElement[];
+    finisher?: <T>(clazz: Constructor<T>) => undefined | Constructor<T>;
+    descriptor?: PropertyDescriptor;
+}
+
+export const consume = <T = object>(providerTag: string, mapFn?: MapFn<T>) =>
+    (classOrDescriptor: Constructor<HTMLElement> | ClassDescriptor) =>
+        (typeof classOrDescriptor === 'function') ?
+            legacyCustomElement<T>(classOrDescriptor, providerTag, mapFn) :
+            standardCustomElement<T>(classOrDescriptor, providerTag, mapFn);
+
+const legacyCustomElement =
+    <T = object>(classConstructor: Constructor<HTMLElement>, providerTag: string, mapFn?: MapFn<T>) => {
+        const consumer = (consumer: HTMLElement, mapFn?: MapFn<T>) => contextConsumer(providerTag, consumer, mapFn);
         return class extends classConstructor {
             connectedCallback() {
                 super.connectedCallback();
                 consumer(this, mapFn);
             }
         }
-    }
-};
+    };
+
+const standardCustomElement =
+    <T = object>(classConstructor: ClassDescriptor, providerTag: string, mapFn?: MapFn<T>) => {
+        const { kind, elements } = classConstructor;
+        const consumer = (consumer: HTMLElement, mapFn?: MapFn<T>) => contextConsumer(providerTag, consumer, mapFn);
+        return {
+            kind,
+            elements,
+            finisher(clazz: Constructor<HTMLElement>) {
+                clazz.prototype.__connectedCallback = clazz.prototype.connectedCallback;
+                clazz.prototype.connectedCallback = function connectedCallbackHandler() {
+                  this.__connectedCallback();
+                  consumer(this, mapFn);
+                };
+            }
+        };
+    };
